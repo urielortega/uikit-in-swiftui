317f4298cd886c5bed899cd0efaf6b5292a03e84
Uriel Ortega <urielortega2509@gmail.com>
2024-10-15 17:03:31 -0600

    Set Initial scaffolding for UITextFieldWrapper.

--- UIKitInSwiftUI/UITextFieldWrapper.swift
+++ UIKitInSwiftUI/UITextFieldWrapper.swift
@@ -8,3 +8,24 @@
 import SwiftUI
 import UIKit
 
+/// A SwiftUI wrapper for `UITextField` that allows customization of placeholder and text binding.
+struct UITextFieldWrapper: UIViewRepresentable {
+    @Binding var text: String
+    let placeholder: String?
+    
+    func makeUIView(context: Context) -> UITextField {
+        return UITextField() // Placeholder to avoid warnings.
+    }
+    
+    func updateUIView(_ uiView: UITextField, context: Context) {
+        
+    }
+    
+    func makeCoordinator() -> Coordinator {
+        return UITextFieldWrapper.Coordinator() // Placeholder to avoid warnings.
+    }
+    
+    class Coordinator: NSObject, UITextFieldDelegate {
+        
+    }
+}
