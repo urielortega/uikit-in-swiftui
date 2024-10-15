729c0af8be9a3d2890e16bff3d1b5ccd6bb41f69
Uriel Ortega <urielortega2509@gmail.com>
2024-10-15 16:55:57 -0600

    Initial Commit

---
+++ UIKitInSwiftUI/UIKitInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIKitInSwiftUIApp.swift
+//  UIKitInSwiftUI
+//
+//  Created by Uriel Ortega on 15/10/24.
+//
+
+import SwiftUI
+
+@main
+struct UIKitInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
